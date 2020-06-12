3c826ea6775314f19f8d229fbe705637f2275142
HMoreira2020 <moreiraheather@gmail.com>
2020-06-11 19:57:47 -0700

    Build '/signup' action, refactor environment.rb file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,13 @@ class UsersController < ApplicationController
     end 
 
     post '/signup' do 
-        raise params.inspect 
+        if params[:trailname].empty? || params[:email].empty? || params[:password].empty?
+            flash[:message] = "All fields are required"
+            redirect to '/signup'
+        else
+            @user = User.create(params)
+            session[:user_id] = @user.id 
+            redirect to '/login'
+        end
     end 
 end 
