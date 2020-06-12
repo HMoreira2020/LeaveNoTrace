2ac38a402b35cb21f35ce166a02e8900efd82019
HMoreira2020 <moreiraheather@gmail.com>
2020-06-11 21:02:38 -0700

    Create new hike form and hikes index page

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,24 +1,37 @@
 class HikesController < ApplicationController 
 
     get '/hikes' do 
-        erb :'hikes/hike_index'
+        # if is_logged_in?
+            @hikes = Hike.all
+            erb :'hikes/hikes_index'
+        # else 
+        #     redirect to '/login'
+        # end
     end 
 
     get '/hikes/new' do 
+        erb :'hikes/new_hike'
     end 
 
     post '/hikes' do 
+        @hike = current_user.hikes.build(params)
+        @hike.save 
     end 
 
     get '/hike/:id' do 
+        @hike = Hike.find_by(params[:id])
     end 
 
     get '/hikes/:id/edit' do 
+        @hike = Hike.find_by(params[:id])
+
     end 
 
     patch '/hikes/:id/edit' do 
+        @hike = Hike.find_by(params[:id])
     end 
 
     delete '/hikes/:id' do 
+        @hike = Hike.find_by(params[:id])
     end 
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base 
     has_many :hikes
     has_secure_password
+
+    def self.find_by_slug(slug)
+        self.all.find {|obj| obj.slug == slug}
+      end 
+    
+      def slug
+        self.username.parameterize
+      end 
 end
