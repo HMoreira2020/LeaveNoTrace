4745d54e6d425b0869a599b8290bf0ce74931f77
HMoreira2020 <moreiraheather@gmail.com>
2020-06-14 16:58:03 -0700

    Remove flash message from post '/login' after  successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,6 @@ class SessionsController < ApplicationController
       @user = User.find_by(trailname: params[:trailname]) 
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id 
-            flash[:notice] = "You are Logged in as #{@user.trailname}!"
             redirect to '/hikes'
         else 
             flash[:warning] = "Invalid trailname or password"
