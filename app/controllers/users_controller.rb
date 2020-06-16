9c93510137055726353504f1a5959ca7f8453b47
HMoreira2020 <moreiraheather@gmail.com>
2020-06-16 11:16:03 -0700

    Remove unneccessary instance variables from controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
     end 
 
     post '/signup' do 
-        @user = User.new(params)
-        if @user.save
+        user = User.new(params)
+        if user.save
             session[:user_id] = @user.id 
             redirect to '/login'
         else 
