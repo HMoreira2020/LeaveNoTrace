c0372708f823fa80f7cd4ca294b76f47187533cc
HMoreira2020 <moreiraheather@gmail.com>
2020-06-15 17:54:25 -0700

    Change redirect to home page upon login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
     get '/login' do 
         if is_logged_in?
-            redirect to '/hikes'
+            redirect to '/home'
         else 
             erb :'sessions/login'
         end
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
       @user = User.find_by(trailname: params[:trailname]) 
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id 
-            redirect to '/hikes'
+            redirect to '/home'
         else 
             redirect to '/login'
         end 
